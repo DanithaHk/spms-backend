6b7efb453959ee6126c798abd77d0b2d98fa7900
DanithaHk <danithahk@gmail.com>
2025-06-25 20:08:52 +0530

    update

--- eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,13 @@
-package lk.ijse.userservice;
+package com.example.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
